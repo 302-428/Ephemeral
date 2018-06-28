72a41310c53fa187188243aeac8b5a8f9c58dccd
tjdans1114 <tjdans1114@snu.ac.kr>
2018-06-28 12:09:01 +0900

    filter binary_op, unary_op

--- extract.py
+++ extract.py
@@ -1,3 +1,59 @@
+from pycparser import c_parser, c_ast
+
+# toy language is as below : without declaration
+# e = const | var | let x e
+#   | e = e (assignment)
+#   | if e e e
+#   | while e e
+#   | {e ; ... ; e} (sequence)
+#   | e b_op e  | u_op e
+#   | empty ( )
+# b_op = +, - , * , / , &&, ||, ==
+# u_op = !
+
+def proper_type(ast):
+    return (type(ast) in [
+        c_ast.Constant, #const
+        c_ast.ID,       #var
+        # c_ast.Decl,     #let x e
+
+        c_ast.Assignment, # e = e
+        c_ast.If,         # if e e e
+        c_ast.While,      # while e e
+        c_ast.Compound,   # {e ; ... ; e}
+
+        c_ast.BinaryOp,   # e b_op e
+        c_ast.UnaryOp,    # e u_op e
+
+        c_ast.EmptyStatement, #( )
+        #c_ast.NamedInitializer, # ???       
+    ]) or (type(ast) in [c_ast.BinaryOp] and ast.op in [
+        "+", "-" , "*" , "/" , "&&", "||", "=="
+    ]) or (type(ast) in [c_ast.UnaryOp] and ast.op in [
+        "!"
+    ])
+
+def report_to_server(ast) :
+    # 1. re-name variables 'in unique way'
+    # 2. record the 'variable semantic' in the database
+    
+    #pass #TODO ################I dont like '#' I prefer /**/
+    return report(ast)
+
+
+def report_children(ast_children_tuple):
+    flag = True
+    for ast_child in ast_children_tuple : 
+        flag = flag and report(ast_child)    
+    return flag
+
+def report(ast) :
+    if(proper_type(ast) and report_children(ast.children())) : # ast.children right?
+        report_to_server(ast)
+        return True
+    else:
+        return False
+    
 #-----------------------------------------------------------------
 # pycparser: explore_ast.py
 #
@@ -28,7 +84,6 @@ struct data {
 	int a;
 	int b;
 };
-
 typedef int Node, Hash;
 void foo(int a, int b) {
 	a = (a*25)+360 / 15;
@@ -77,9 +132,9 @@ def proper_type(ast):
 		return True
 	if(T is pycparser.c_ast.Compound):
 		return True
-	if(T is pycparser.c_ast.BinaryOp):
+	if(T is pycparser.c_ast.BinaryOp and ast.op in ["+", "-" , "*" , "/" , "&&", "||", "=="]):
 		return True
-	if(T is pycparser.c_ast.UnaryOp):
+	if(T is pycparser.c_ast.UnaryOp and ast.op in ["!"]):
 		return True
 	if(T is pycparser.c_ast.BinaryOp):
 		return True
@@ -110,4 +165,3 @@ report(ast)
 for i in xrange(0, len(astlist)):
 	print("Printing AST")
 	astlist[i].show(showcoord=True)
-
